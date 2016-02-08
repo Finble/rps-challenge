c81631942c90bd8c3591b2a72cdf2c3a06e10ce3
Finble <elainefinn@hotmail.com>
2016-02-08 14:11:16 +0000

    Player reviews options

--- spec/features/features_spec.rb
+++ spec/features/features_spec.rb
@@ -1,3 +1,5 @@
+#User Story 1 - enter name
+
 feature 'Enter names' do
 
 		# before do
@@ -11,10 +13,23 @@ feature 'Enter names' do
 	end
 end
 
-feature 'Submit choice' do
 
-	scenario 'see markateer submit choice' do
-		sign_in_and_play
-		expect(page).to have_content 'Elaine: Paper'   
+
+feature 'Play the game' do
+
+	before do
+		visit('/')
+		fill_in :markateer_name, with: 'Elaine' 
+		click_button 'Submit'  
+	end
+
+#User Story 2 - review the options
+
+	scenario 'see the options' do
+
+		expect(page).to have_content 'Rock'
+		expect(page).to have_content 'Paper'
+		expect(page).to have_content 'Scissors'
+
 	end
 end
