fd62c0c89bc51d5a27d92eb41099947505591ced
Finble <elainefinn@hotmail.com>
2016-02-08 12:44:35 +0000

    Enabled sessions, redirected post to get route (so as not to render instance var from post)

--- app.rb
+++ app.rb
@@ -1,15 +1,23 @@
 require 'sinatra/base'
 
 class RPS < Sinatra::Base
-  get '/' do
-    erb :index
-  end
+enable :sessions
 
-post '/names' do
-  @player_1_name = params[:player_1_name]
-  @player_2_name = params[:player_2_name]
-  erb :play
-end
+	get '/' do
+	  erb :index
+	end
+
+	post '/names' do
+		session[:player_1_name] = params[:player_1_name]  #shouldn't render @player_1_name from post - need to save instance variable in a get
+	  session[:player_2_name] = params[:player_2_name]  #shouldn't render @player_2_name from post - need to save instance variable in a get
+	  redirect '/play'
+	end
+
+	get '/play' do
+		@player_1_name = session[:player_1_name] 
+		@player_2_name = session[:player_2_name]
+		erb :play
+	end
 
   # start the server if ruby file executed directly
   run! if app_file == $0
